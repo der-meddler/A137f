3beead7c93948c225aca26afd984d6017e011efa
thatpainterfromaustria <njilasalexander36@gmail.com>
2026-06-19 11:40:08 +0200

    Update build_kernel.sh for additional config files

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ export ARCH=arm64
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 export CONFIG_DRV_BUILD_IN=Y
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a13ve_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a13ve_defconfig droidspaces.config scamsung.config
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
